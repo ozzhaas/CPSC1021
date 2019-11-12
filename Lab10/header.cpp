3de0dd3f0efc30e8991e589a69bea21becfcdee4
Kellen Haas <mkhaas@cerf19.computing.clemson.edu>
2019-11-12 16:26:27 -0500

    11.12 4:25pm

---
+++ Lab10/header.cpp
@@ -0,0 +1,73 @@
+/**********************
+  Kellen Haas
+  CPSC1021
+  Sec. 001-F19
+  mkhaas@g.clemson.edu
+  Damion
+  11/12/19
+ *********************/
+
+
+
+Header(string m = " ", int w = 0, int h = 0, int mv = 255) {
+
+
+
+
+}
+
+
+void setMagic(string) {
+
+
+
+}
+
+
+void setWidth(int) {
+
+
+
+}
+
+
+void setHeight(int) {
+
+
+}
+
+
+void setMaxVal(int) {
+
+
+}
+
+
+string getMagic()const {
+
+}
+
+
+int getWidth()const {
+
+}
+
+
+int getHeight()const {
+
+}
+
+
+int getMVal()const {
+
+}
+
+
+void readHeader(ifstream&) {
+    
+}
+
+
+void writeHeader(ofstream&) {
+
+}
---
+++ Lab10/vec2DDriver.cpp
@@ -0,0 +1,8 @@
+/**********************
+  Kellen Haas
+  CPSC1021
+  Sec. 001-F19
+  mkhaas@g.clemson.edu
+  Damion
+  11/12/19
+ *********************/
