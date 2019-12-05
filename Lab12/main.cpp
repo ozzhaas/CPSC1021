acc5d2a76426af9135619d80bdf470380048141d
Kellen Haas <mkhaas@cerf19.computing.clemson.edu>
2019-12-05 16:28:08 -0500

    12.5 4:28pm

--- Lab12/main.cpp
+++ Lab12/main.cpp
@@ -16,7 +16,10 @@ void rem();
 //Delete the list//
 void deleteList();
 
+//Adding to the front of the list//
+void add_to_front();
 
+//Print the menu//
 int printMenu();
 
 
@@ -41,8 +44,10 @@ int main() {
         //Call all functions here//
         kill = printMenu();
         if (kill == 1) add();
-        else if (kill == 2) print();
-
+        else if (kill == 2) add_to_front();
+        else if (kill == 3) rem();
+        else if (kill == 4) print();
+        else if (kill == 5) deleteList();
     }
 
 return 0;
@@ -52,8 +57,12 @@ return 0;
 int printMenu() {
     int userInput = 0;
 
-    cout << "1. for add\n2. for print\n-1 to quit program\n";
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    cout << "1. for add\n2. for add to the front\n3. for remove\n4. for print\n5. for deleting the list\n-1 to quit program\n";
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
     cin >> userInput;
+    cout << "---\n";
 
     return userInput;
 }
@@ -63,8 +72,10 @@ int printMenu() {
 void add() {
     int userInput = 0;
 
+    cout << "\n";
     cout << "What number do you want to add?\n";
     cin >> userInput;
+    cout << "\n";
 
     //Base case
     if (head->next == nullptr) {
@@ -84,30 +95,62 @@ void add() {
 
 void print() {
 
+    cout << "\n";
+    cout << "Numbers: \n";
     for (Node* p = head->next; p != NULL; p = p->next) {
         cout << p->number << " ";
     }
-    cout << endl;
-
+    cout << "\n";
+    cout << "\n";
 
 }
 
 
 void rem() {
     int remNum = 0;
+    cout << "\n";
     cout << "What number do you want to remove?\n";
     cin >> remNum;
+    cout << "\n";
 
-    for (Node* r = head->next; r != NULL; r = r->next) {
-        if (*r == remNum){
-            Node* prevNode = head;
-            prevNode = prevNode->next;
+
+    for (Node* r = head; r != NULL; r = r->next) {
+        if (r->next == NULL) break;
+
+        else if (r->next->number == remNum) {
+            Node* tempNode = r->next;
+            r->next = r->next->next;
+            delete tempNode;
         }
 
     }
 }
 
 
+void add_to_front() {
+    int userInput = 0;
+    cout << "\n";
+    cout << "What number do you want to add to the front of the list?\n";
+    cin >> userInput;
+    cout << "\n";
+
+    Node* tempNode = new Node(userInput);
+    tempNode->next = head->next;
+    head->next = tempNode;
+
+}
+
+
+
 void deleteList() {
+
+
+    for (Node* d = head; d != NULL; d = d->next) {
+        if (d->next == NULL) {
+            delete d;
+        }
+
+    }
+    head = NULL;
 
 }
